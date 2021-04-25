c0c03352a4eb0f6f7718db35251f766037662d6e
fwz7mmm <43370471+fwz7mmm@users.noreply.github.com>
2021-04-25 20:35:20 +0800

    xin

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from app import db
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    email = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128))
+    phone = db.Column(db.Integer)
+    birth = db.Column(db.String(50))
+    status = db.Column(db.Boolean)
+    last_login = db.Column(db.DATE, nullable=False, default=datetime.now)
+
+# Printing out which user is current
+    def __repr__(self):
+        return '<UserId {0}, email {1}, name {2}> last_login {3}'.format(self.id, self.email, self.name, self.last_login)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+import sqlite3
+
+class Config(object):
+
+    # Creation of secret key for WTForms
+    SECRET_KEY = os.environ.get('SERCRET_KEY') or "123-key"
+
+    # Set specs for SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
