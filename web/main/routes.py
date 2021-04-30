a51298dae110e307b2e623ef440adf4054a39acc
w121wwwww <22906827@student.uwa.edu.au>
2021-04-30 15:08:01 +0800

    Revert "Merge branch 'master' of https://github.com/fwz7mmm/Group-Project"
    This reverts commit 29f91efe0700434c91caece09fa47d8fd18c40ee, reversing
    changes made to 61011412e908e97e1d36f46b4d86307ec4baed9e.

--- web/main/routes.py
+++ web/main/routes.py
@@ -1,6 +1,5 @@
 from flask import Flask,render_template, url_for,Blueprint
 from web import get_logger,bcrypt,db
-from flask_sqlalchemy import SQLAlchemy
 logger = get_logger(__name__)
 main = Blueprint('main', __name__)
 
@@ -11,5 +10,4 @@ def index():
 @main.route("/home")
 def home():
     return render_template("home.html")
-    
-    
+
